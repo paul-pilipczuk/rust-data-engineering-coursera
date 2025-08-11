fe9af2a61f9f791d7a64914b2a2c1332a5dfa3ef
Paul Pilipczuk <paul.pilipczuk.20@cnu.edu>
2025-08-11 11:40:11 -0400

    added in docs for basic structs

---
+++ week_1/print-data-structs/src/main.rs
@@ -0,0 +1,23 @@
+fn main() {
+    println!("Common rust Data Structures");
+
+    // Sequences
+    println!("\n\tSequences:");
+    println!("\n\tVec: https://doc.rust-lang.org/std/vec/struct.Vec.html");
+    println!("\n\tVecDeque: https://doc.rust-lang.org/std/collections/struct.VecDeque.html");
+    println!("\n\tLinkedList: https://doc.rust-lang.org/std/collections/struct.LinkedList.html");
+
+    // Maps
+    println!("\n\tMaps:");
+    println!("\n\tHashMap: https://doc.rust-lang.org/std/collections/struct.HashMap.html");
+    println!("\n\tBTreeMap: https://doc.rust-lang.org/std/collections/struct.BTreeMap.html");
+
+    // Sets
+    println!("\n\tSets:");
+    println!("\n\tHashSet: https://doc.rust-lang.org/std/collections/struct.HashSet.html");
+    println!("\n\tBTreeSet: https://doc.rust-lang.org/std/collections/struct.BTreeSet.html");
+
+    // Misc
+    println!("\n\tMisc:");
+    println!("\n\tBinaryHeap: https://doc.rust-lang.org/std/collections/struct.BinaryHeap.html");
+}
